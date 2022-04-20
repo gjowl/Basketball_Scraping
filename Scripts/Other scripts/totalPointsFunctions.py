fe647dfa9edb11917d7fdb1b76646965392920a6
gjowl <gjowl04@gmail.com>
2022-04-19 23:57:46 -0500

    slight changes to prepping for bball reference scraping

--- Scripts/Other scripts/totalPointsFunctions.py
+++ Scripts/Other scripts/totalPointsFunctions.py
@@ -13,7 +13,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def scrape_NBA_team_data(years = [2017, 2018]):
+def scrapeTotalPoints():
+    #TODO: make a way to get every single player from every single team ever and scrape the total points scored value
+    # may be a little difficult: have to go to url for each player? or can I type a search into the bar or something?
+    # I got a csv file of data from bballreference
     final_df = pd.DataFrame(columns = ["Year", "Team", "W", "L",
                                        "W/L%", "GB", "PS/G", "PA/G",
                                        "SRS", "Playoffs",
