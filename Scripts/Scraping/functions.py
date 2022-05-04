4ea6e56608f5bc18c0182f191d9048f914f291c2
gjowl <gjowl04@gmail.com>
2022-05-03 23:10:29 -0500

    need to get selenium working

--- Scripts/Scraping/functions.py
+++ Scripts/Scraping/functions.py
@@ -1,5 +1,8 @@
-
 from urllib.request import urlopen
+
+# https://github.com/microsoft/WSL/issues/5126: helped with problems installing chromium
+# which was necessary for using chromedriver
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import player
@@ -15,18 +18,26 @@ def makeOutputDir(outputDir):
         print('Output Directory: ' + outputDir + ' exists.')
 
 def getTableNames(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
+    print('worked')
+    exit()
     # collect HTML data
     html = urlopen(url)
     # create beautiful soup object from HTML
     soup = BeautifulSoup(html, features="lxml")
-
-    # get all the tables from the page
+    tables = []
     tables = soup.find_all('table')
     #div = soup.find_all('div', class_='table_wrapped')
     tableIds = []
     for table in tables:
         id = table.get('id')
+        print(id)
         tableIds.append(id)
+    exit()
     return tableIds
 
 def getTablesAsDataframes(tableNames, url):
@@ -39,22 +50,15 @@ def getTablesAsDataframes(tableNames, url):
             df_dict[name] = df_data #.append to update
     return df_dict
 
-def scrapeAllPlayers():
-    #TODO: make a way to get every single player from every single team ever and scrape the total points scored value
-    # may be a little difficult: have to go to url for each player? or can I type a search into the bar or something?
-    # I got a csv file of data from bballreference: playersFromBasketballReference.csv
-    dataDir = "/mnt/c/Users/gjowl/github/Basketball_Scraping/Data files/"
-    playersCsv = dataDir+"playersUpdated.csv"
-    df = pd.read_csv(playersCsv, sep=",")
-
-    scrapeDataDir = "/mnt/h/Basketball_Reference/"
-    makeOutputDir(scrapeDataDir)
+def scrapeAllPlayers(playersCsv, saveDir):
+    # convert csv file to dataframe
+    df_players = pd.read_csv(playersCsv, sep=",")
     
     # URL to scrape
     playersUrl = "https://www.basketball-reference.com/players/"
 
     # getter function
-    for name, url in zip(df.PlayerName, df.Url):
+    for name, url in zip(df_players.PlayerName, df_players.Url):
         #TODO: it will lock me out if I have too many requests per second, so I'll have to add in a wait time
         # append player url with url of each player
         urlToSearch = playersUrl+url
@@ -65,10 +69,9 @@ def scrapeAllPlayers():
         # convert the types of tables using beautiful soup
         # get all of the dataframes from the webpage (currently only gets 6 (per ones I can't seem to pull out yet))
         df_dict = getTablesAsDataframes(tableIds, urlToSearch)
-        
         print(name)
         
-        dataFile = scrapeDataDir+'/' + name + '.csv'
+        dataFile = saveDir+'/' + name + '.csv'
         
         # loop through all dataframes 
         for id, df in df_dict.items():
@@ -82,7 +85,7 @@ def scrapeAllPlayers():
             #    for col in colNames:
             #        data = df.at[index,col]
             #        print(data)
-
+        exit()
         #TODO: think of good ways to save this data for all players: name of the spreadsheet is the name of the table?
         # I just decided to read about the terms of use, and looks like I can't actually create a website using this data (and maybe not even results of this data? Not sure though.
         # At the very least, I can blog/create content about this stuff as I find it using data from basketball reference as a source?) And this scraper is a nice piece of software
@@ -91,15 +94,3 @@ def scrapeAllPlayers():
         # use getText()to extract the headers into a list
         titles = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
         print(titles)
-
-        #per_game = soup.find("table", {"id":'per_game'})
-
-        #notes on automation for this script:
-        """
-        for everyday:
-            - scrape current list against the new list of players from bball reference and compare; if new, flag
-            - scrape data for players who are playing the day before
-            - scrape boxscores; if new, flag
-            - scrape data for the current 450ish players who are in the league every day
-                - for efficiency, maybe make a csv to update for the year? Not for all data
-        """
--- Scripts/Scraping/main.py
+++ Scripts/Scraping/main.py
@@ -1,5 +1,14 @@
 from functions import *
 
+#TODO: make a way to get every single player from every single team ever and scrape the total points scored value
+# may be a little difficult: have to go to url for each player? or can I type a search into the bar or something?
+# I got a csv file of data from bballreference: playersFromBasketballReference.csv
+dataDir = "/mnt/c/Users/gjowl/github/Basketball_Scraping/Data files/"
+playersCsv = dataDir+"playersUpdated.csv"
+
+scrapeDataDir = "/mnt/h/Basketball_Reference/"
+makeOutputDir(scrapeDataDir)
+
 #Main
 if __name__ == '__main__':
-    scrapeAllPlayers() 
+    scrapeAllPlayers(playersCsv, scrapeDataDir) 
