27ff843b38639b336fbf3cf351cdbfc3d4c33b15
gjowl <gjowl04@gmail.com>
2022-07-04 11:57:18 -0500

    runs; need to get rest of per_mode and seasons

--- Scripts/NBA_API/main.py
+++ Scripts/NBA_API/main.py
@@ -1,15 +1,18 @@
 import requests
 import pandas as pd
+import time
+import os
+import random
 """
 The below code is adapted from from the following source:
 LearnWithJabe: https://www.youtube.com/watch?v=IELK56jIsEo&ab_channel=LearnWithJabe
 """
-
-# TODO: this may have to be hardcoded? I don't know how else to get a list of these from NBA.com
-per_mode = 'Totals'
-# TODO: make the season list dynamic; Jabe hard-coded the season list
-season_id = '2018-19'
-player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode='+per_mode+'&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season='+season_id+'&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='
+# wait for a random amount of time between 1 and 10 seconds (to avoid being blocked as a bot)
+def wait():
+    time.sleep(1 + (10 * random.random()))
+per_mode_list = ['Totals']
+#season_id_list = ['2015-16', '2016-17', '2017-18', '2018-19', '2019-20', '2020-21', '2021-22']
+season_id_list = ['2020-21']
 # need this to request through NBA; from this: https://github.com/rd11490/NBA_Tutorials/tree/master/finding_endpoints
 headers  = {
     'Connection': 'keep-alive',
@@ -23,14 +26,28 @@ headers  = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
 }
-response = requests.get(url=player_info_url, headers=headers).json()
-# Jabe hard-coded column names; I figured out the below to get the column names from the webpage
-columnNames = response['resultSets'][0]['headers']
-player_info = response['resultSets'][0]['rowSet']
-nba_df = pd.DataFrame(player_info)
-
-# set column names of the nba_df to columnNames
-nba_df.columns = columnNames
-print(nba_df.sample(10))
-
-#nba_df.to_csv('nba_df.csv', index=False)
+# set up the output directory to the current directory
+output_dir = os.getcwd()
+for per_mode in per_mode_list:
+    for season_id in season_id_list:
+        player_file = f"{output_dir}_{per_mode}_{season_id}.csv"
+        column_names_file = f"{output_dir}_{per_mode}_{season_id}.csv"
+        all_data_file = f"{output_dir}_{per_mode}_{season_id}.csv"
+        wait()
+        player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode='+per_mode+'&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season='+season_id+'&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='
+        # get the data from the url        
+        response = requests.get(url=player_info_url, headers=headers).json()
+        # Jabe hard-coded column names; I figured out the below to get the column names from the webpage
+        column_names = response['resultSets'][0]['headers']
+        player_info = response['resultSets'][0]['rowSet']
+        nba_df = pd.DataFrame(player_info)
+        # set column names of the nba_df to columnNames
+        nba_df.columns = column_names
+        # get all of the player names
+        players = nba_df['PLAYER_NAME']
+        # save the player names to a csv file
+        players.to_csv(player_file, index=False)
+        # save the column names to a csv file
+        #column_names.to_csv(column_names_file, index=False)
+        # save the nba_df to a csv file
+        nba_df.to_csv(all_data_file, index=False)
