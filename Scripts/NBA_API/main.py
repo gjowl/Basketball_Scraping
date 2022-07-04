dbe386e1c463695892a9771832c98648fd1a862c
gjowl <gjowl04@gmail.com>
2022-07-03 22:36:48 -0500

    retrieves data; need to add dynamic modes

--- Scripts/NBA_API/main.py
+++ Scripts/NBA_API/main.py
@@ -1,13 +1,14 @@
 import requests
-from settings import Settings
-from models import PlayerGeneralTraditionalTotals
-
+import pandas as pd
 """
 The below code is adapted from from the following source:
 LearnWithJabe: https://www.youtube.com/watch?v=IELK56jIsEo&ab_channel=LearnWithJabe
 """
-settings = Settings()
-settings.db.create_tables([PlayerGeneralTraditionalTotals], safe=True)
+
+# TODO: this may have to be hardcoded? I don't know how else to get a list of these from NBA.com
+per_mode = 'Totals'
+# TODO: make the season list dynamic; Jabe hard-coded the season list
+season_id = '2018-19'
 player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode='+per_mode+'&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season='+season_id+'&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='
 # need this to request through NBA; from this: https://github.com/rd11490/NBA_Tutorials/tree/master/finding_endpoints
 headers  = {
@@ -23,8 +24,13 @@ headers  = {
     'Accept-Language': 'en-US,en;q=0.9',
 }
 response = requests.get(url=player_info_url, headers=headers).json()
+# Jabe hard-coded column names; I figured out the below to get the column names from the webpage
+columnNames = response['resultSets'][0]['headers']
 player_info = response['resultSets'][0]['rowSet']
-print(player_info)
+nba_df = pd.DataFrame(player_info)
 
-# TODO: make getting column names for this more dynamic; Jabe hard-coded the column names
-# TODO: make the season list dynamic; Jabe hard-coded the season list
+# set column names of the nba_df to columnNames
+nba_df.columns = columnNames
+print(nba_df.sample(10))
+
+#nba_df.to_csv('nba_df.csv', index=False)
