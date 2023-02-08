a84ebd0333efac266e8e21bb9212ccf0a6a65568
Gilbert Loiseau <gjowl04@gmail.com>
2023-02-08 16:36:35 -0600

    update functions; calculates usage rate

--- Scripts/NBA_API/classes/boxScore.py
+++ Scripts/NBA_API/classes/boxScore.py
@@ -1,6 +1,6 @@
 import os, sys, pandas as pd
-from classes.teamScore import teamScore
-from classes.playerScore import playerScore 
+from classes.teamBoxScore import teamBoxScore
+from classes.playerBoxScore import playerBoxScore 
 
 # initializes an class object to hold the data file. Contains simple functions for handling data
 class boxScore:
@@ -35,17 +35,49 @@ class boxScore:
     def imposeLimit(self, colName, limit):
         self.box = self.box[self.box[colName] > limit]
 
+    # calculate usage rate
+    # https://www.reddit.com/r/nba/comments/5p7h1g/lets_talk_about_usage_rate_and_how_to_use_it/
+    # 100 * ((FGA + 0.44 * FTA + TOV) * (Tm MP / 5)) / (MP * (Tm FGA + 0.44 * Tm FTA + Tm TOV))
+    def calcUsage(self):
+        df = self.box
+        # get the team data for calculating usage rate
+        cols = ['MIN', 'FGA', 'FTA', 'TOV']
+        df = self.getTeamData(df, cols)
+        # put the minutes, field goals attempted, free throws attempted, turnovers, and team stats into variables
+        minutes, fga, fta, asts, tov = df['MIN'], df['FGA'], df['FTA'], df['AST'], df['TOV']
+        teamFga, teamFta, teamTov, teamMin = df['TEAM_FGA'], df['TEAM_FTA'], df['TEAM_TOV'], df['TEAM_MIN']
+        # calculate usage rate
+        df['USG%'] = 100 * ((fga + 0.44 * fta + tov) * (teamMin/5)) / (minutes * (teamFga + 0.44 * teamFta + teamTov))
+        # if minutes < 10, set usage rate to 0
+        df.loc[df['MIN'] < 10, 'USG%'] = 0
+        self.box = df
+
+    # helper function for calculating player usage rate; can also be used to get other team stats for calculating percentages 
+    # (i.e. field goal attempt % per player = FGA / TEAM_FGA, etc.)
+    def getTeamData(self, df, cols):
+        outputDf = pd.DataFrame()
+        # loop through unique TEAM_ABBREVIATION
+        for team in df['TEAM_ABBREVIATION'].unique():
+            teamDf = df[df['TEAM_ABBREVIATION'] == team].copy()
+            for col in cols:
+                # get the data for the team
+                teamDf['TEAM_' + str(col)] = teamDf.loc[teamDf['TEAM_ABBREVIATION' == team, col]].sum()
+            outputDf = pd.concat([outputDf, teamDf])
+        return outputDf
+
     # returns the boxscore dataframe
     def getBoxScore(self):
         return self.box
 
-    def getTeamScore(self, team):
-        score = teamScore(self.box)
+    # return the data for a team into a teamData object
+    def getTeamBoxScore(self, team):
+        score = teamBoxScore(self.box)
         score.setTeam(team)
         return score
 
-    def getPlayerScore(self, player):
-        score = playerScore(self.box)
+    # return the data for a player into a playerData object
+    def getPlayerBoxScore(self, player):
+        score = playerBoxScore(self.box)
         score.setPlayer(player)
         return score
 
