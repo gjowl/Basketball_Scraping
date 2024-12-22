7e44018b7bff4de97b2e8b3f95468473bd51231d
gjowl <gjowl04@gmail.com>
2024-12-22 18:16:09 -0500

    beginner data in; prep for next steps to work on while abroad

--- site/pages/1_🔝_2024-2025 season player stats.py
+++ site/pages/1_🔝_2024-2025 season player stats.py
@@ -18,53 +18,21 @@ datadir = '/mnt/h/NBA_API_DATA/BOXSCORES/2024-12-20'
 contains = 'all_game' # file you want to read
 
 # FUNCTIONS
-#def make_spec(_xCol, _yCol, _labelCol):
-#    # check if col name has a _ in it
-#    if '_' in _xCol:
-#        xColName = _xCol.split('_')[0]
-#    elif 'PG' in _xCol:
-#        xColName = _xCol[:-2]
-#    if '_' in _yCol:
-#        yColName = _yCol.split('_')[0]
-#    elif 'PG' in _yCol:
-#        yColName = _yCol[:-2]
-#    spec = {
-#        'mark': {'type': 'circle', 'tooltip': True},
-#        'params': [
-#            {'name': 'interval_selection', 'select': 'interval'},
-#            {'name': 'point_selection', 'select': 'point'}
-#        ],
-#        # change the axis labels
-#        'encoding': {
-#            'x': {'field': _xCol, 'type': 'quantitative'},
-#            'y': {
-#                'field': _yCol, 'type': 'quantitative', 'scale': {'zero': False}, 'title': yColName, 'axis': {'labelOverlap': 'parity'}, 'labelAngle': 90
-#                },
-#            'color': {'field': _labelCol, 'type': 'nominal'},
-#            'fillOpacity': {
-#                'condition': {
-#                    'param': 'point_selection', 'value': 1},
-#                    'value': 0.3,
-#                },
-#            'legend': {'title': 'Player Name'},
-#            'layout': {'columns': 2},
-#            'config': {'axisY': {'minExtent': 40, 'labelAngle': 100}, 'view': {'stroke': 'transparent'}},
-#            },
-#    }
-#    return spec
 
-def concat_col_names(_data, _col1, _col2):
-#    # check if col name has a _ in it
-#    if '_' in _xCol:
-#        xColName = _xCol.split('_')[0]
-#    elif 'PG' in _xCol:
-#        xColName = _xCol[:-2]
-#    if '_' in _yCol:
-#        yColName = _yCol.split('_')[0]
-#    elif 'PG' in _yCol:
-#        yColName = _yCol[:-2]
-    _data['newCol'] = _data[_col1] / _data[_col2]
-    return
+# concats the column names, removing the _G and PG 
+def concat_col_names(_xCol, _yCol):
+    # check if col name has a _ in it
+    if '_' in _yCol:
+        yColName = _yCol.split('_')[0]
+    elif 'PG' in _yCol:
+        yColName = _yCol[:-2]
+    if '_' in _xCol:
+        xColName = _xCol.split('_')[0]
+        colName = f'{xColName}_{yColName}'
+    elif 'PG' in _xCol:
+        xColName = _xCol[:-1]
+        colName = f'{xColName}{yColName}'
+    return colName
 
 # sort and show the data
 def sort_and_show_data(_data, _col1, _col2, n=10):
@@ -72,29 +40,22 @@ def sort_and_show_data(_data, _col1, _col2, n=10):
     top = _data.sort_values(by=_col1, ascending=False).head(n)
     top = top.reset_index(drop=True)
     # trim to only have player name and the stat
-    top = top[['PLAYER_NAME', _col1, f'{_col1}_Percentile', _col2]]
+    percentile_col = f'{_col1}_Percentile'
+    top = top[['PLAYER_NAME', _col1, _col2, percentile_col]]
     #top = top[['PLAYER_NAME', _col1, _col2]]
-    newCol = f'{_col1}_per_{_col2}'
+    newCol = concat_col_names(_col1, _col2)
     top[newCol] = top[_col1] / top[_col2]
+    # normalize the data
     if st.button(f'{_col1}', key=f'{_col1}_button'):
         st.write(top)
-        # make a bar graph of percentile
-        st.write(f'{_col1} Percentile')
         # make spec for vega-lite charts
-        fig1 = px.scatter(top, x=_col2, y=_col1, color='PLAYER_NAME', title=f'{_col1} vs {_col2}', labels={'x': _col2, 'y': _col1})
-        fig2 = px.scatter(top, x=_col1, y=newCol, color='PLAYER_NAME', title=f'{_col1} vs {newCol}', labels={'x': _col1, 'y': newCol})
+        fig1 = px.scatter(top, x=_col2, y=_col1, color='PLAYER_NAME', title=f'{_col2} vs {_col1}', labels={'x': _col2, 'y': _col1}, size=f'{percentile_col}')
+        fig2 = px.scatter(top, x=_col1, y=newCol, color='PLAYER_NAME', title=f'{_col1} vs {newCol}', labels={'x': _col1, 'y': newCol}, size=f'{percentile_col}')
         c1, c2 = st.columns(2)
         with c1:
             st.plotly_chart(fig1, use_container_width=False)
         with c2:
             st.plotly_chart(fig2, use_container_width=False)
-        #spec1 = make_spec(_col2, _col1, 'PLAYER_NAME') # scatter of stat2 vs stat1
-        #spec2 = make_spec(_col1, newCol, 'PLAYER_NAME') # scatter of stat1 vs (stat1 per stat2); ex. point per minute, oreb per minute, etc.
-        ## make a scatter plot of the stat vs MPG
-        #st.vega_lite_chart(top, spec1)
-        #st.vega_lite_chart(top, spec2)
-        ##st.write(f'{_col1} vs MPG')
-        ##st.scatter_chart(top, x=_col1, y=_col2, x_label=_col1, y_label=_col2)
         st.button(f'Hide')
 
 
