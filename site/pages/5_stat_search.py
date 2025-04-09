e2e758c5d6c11e5b85f7c99380e00b29daaae168
gjowl <gjowl04@gmail.com>
2025-04-08 21:19:09 -0400

    working on plots for shooting stats; added in FGM_GP & FTM_GP

--- site/pages/5_stat_search.py
+++ site/pages/5_stat_search.py
@@ -83,58 +83,65 @@ with c2:
 #file2 = st.selectbox('Select the year of interest', list(year_data_dict1.keys()))
 #player2 = st.selectbox('Select the player to load', list(year_data_dict[file1]['PLAYER_NAME'].unique()))
 #player = st_searchbox(year_data_dict[file1]['PLAYER_NAME'].unique(), label='Search for a player', placeholder='Search for a player', key='player_searchbox', default_value='')
-# output the player stats
-st.write(year_data_dict[file1][year_data_dict[file1]['PLAYER_NAME'] == player])
 
 # get the data for the player from all years they played in the league
 player_df = get_player_data(year_data_dict, player)
-
-st.write(player_df)
+if st.button('Show Data'):
+    st.write(player_df)
+    st.button('Hide Data')
 
 # TODO: add in a st.toggle here to show different versions of data (ex. turning on/off gp threshold)
-
 name_and_year = ['PLAYER_NAME', 'YEAR']
-percent = ['FG%', '3P%', 'FT%']
-shots = ['FG%', 'FGA_PG', 'FTA_PG', '2P%', '2PA_PG', '2PM_PG', '3P%', '3PA_PG', '3PM_PG', 'PF_PG']
-traditional = ['MPG', 'PPG', 'APG', 'RPG', 'SPG', 'BPG', 'OREB_PG', 'DREB_PG', 'TOV_PG']
+percent = ['FG%', '2P%', '3P%', 'FT%']
+shots = ['FGA_PG', 'FGM_PG', '2PA_PG', '2PM_PG', '3PA_PG', '3PM_PG', 'FTA_PG', 'FTM_PG'] # make into quadrant plots
+traditional = ['MPG', 'PPG', 'APG', 'RPG', 'SPG', 'BPG', 'OREB_PG', 'DREB_PG', 'TOV_PG', 'PF_PG'] # unsure yet
 #advanced = ['AST_TO', 'NBA_FANTASY_PTS_PG', 'TS%', 'USG%', 'OREB%', 'DREB%', 'AST%', 'STL%', 'BLK%']
 # keep all the shooting stats and player name and year
 percent_df = player_df[name_and_year + percent]
 # remove the part after the - from year
 percent_df['YEAR'] = percent_df['YEAR'].str.split('-').str[0]
+shots_df = player_df[name_and_year + shots]
+traditional_df = player_df[name_and_year + traditional]
 
 # list of tabs
 tab1, tab2, tab3 = st.tabs(['Percent', 'Shooting', 'Traditional'])
 with tab1:
     st.header('Percent Stats')
-    st.write('Below are the shooting percentages for the player')
-    st.write(percent_df)
-    for col in percent_df.columns[2:]:
-        fig = px.bar(percent_df, x='YEAR', y=col, title=f'{player} {col}')
-        # write the year exactly as it is in the dataframe
-        st.plotly_chart(fig, use_container_width=True)
+    if st.button('Show Percent Data'):
+        st.write('Below are the shooting percentages for the player')
+        st.write(percent_df)
+    #for col in percent_df.columns[2:]:
+    #    fig = px.bar(percent_df, x='YEAR', y=col, title=f'{player} {col}')
+    #    # write the year exactly as it is in the dataframe
+    #    st.plotly_chart(fig, use_container_width=True)
     fig_list = []
-    # plot a violin plot with the points overlaid
+    # plot a violin plot with the points overlaid for each stat
     for stat in percent:
         fig = go.Figure(data=go.Violin(y=percent_df[stat], x=percent_df['PLAYER_NAME'], box_visible=True, line_color='black', fillcolor='orange', meanline_visible=True, points='all', pointpos=0, opacity=0.6, showlegend=False, x0=stat))
         fig.update_traces(marker=dict(size=5, color='black', line=dict(width=1, color='black')))
         update_yaxis(fig, percent_df, stat)
         fig_list.append(fig)
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     fig1 = fig_list[0]
     fig2 = fig_list[1]
     fig3 = fig_list[2]
+    fig4 = fig_list[3]
     with col1:
         st.plotly_chart(fig1, use_container_width=True)
     with col2:
         st.plotly_chart(fig2, use_container_width=True)
     with col3:
         st.plotly_chart(fig3, use_container_width=True)
-# currently just hardcoding; but I think I should try to find a better way to do this
+    with col4:
+        st.plotly_chart(fig4, use_container_width=True)
+# currently just hardcoding; but I think I should try to find a better way to do the above?
 with tab2:
     st.header('Shooting Stats')
-    st.write('Below are the shooting stats for the player')
-    st.write(player_df[name_and_year + shots])
+    # make a scatterplot of the shooting stats
+    fig = px.scatter(shots_df, x='FGA_PG', y='FGM_PG', color='YEAR', hover_name='PLAYER_NAME', title=f'{player} Shooting Stats')
+    fig.update_traces(marker=dict(size=10, line=dict(width=2, color='black')))
+    fig.update_layout(xaxis_title='FGA PG', yaxis_title='FGM PG')
+    st.plotly_chart(fig, use_container_width=True)
     # I think I'll do something similar here as the above
 with tab3:
     st.header('Traditional Stats')
