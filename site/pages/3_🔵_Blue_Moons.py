5a260c9b91e2d395892dd77804d26774154f4e9d
gjowl <gjowl04@gmail.com>
2025-04-12 13:08:07 -0400

    going to set this page up tomorrow on last day of season while watching games

--- site/pages/3_🔵_Blue_Moons.py
+++ site/pages/3_🔵_Blue_Moons.py
@@ -10,19 +10,21 @@ st.set_page_config(page_title='Blue Moons!',
 st.title('In the works!')
 
 # rare events...what can I add here? 
-# this may at first just be a list of things that could change, and whenever they do, they get updated here
-# Most 3pt attempts in a game
+# Most 3pt attempts in a game 
 # Most 3pt makes in a game
-# Most assists in a game
+# Most assists in a game 
 # Most rebounds in a game
 # Most steals in a game
 # Most blocks in a game
 # Most turnovers in a game
-# Most fouls in a game
 # Most points in a game
 # Most minutes in a game
+
+# The unlikely to ever happen ones
+# Most fouls in a game
 # Most games played in a season
 # Most games played in a career
 # Most games played in a month
 
 # script compares to the current csv file, checks these cats, then updates the main csv if is different (adds in the date, previous change, etc.)
+# this is mostly just a look up table; so I think I'm just going to get those tomorrow when all games are going
