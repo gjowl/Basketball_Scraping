82739fd2ace849d82f327a3b7064eea3511e5e35
gjowl <gjowl04@gmail.com>
2025-04-20 17:44:32 -0400

    a fun idea of solving toxic reddit discussions

---
+++ site/pages/7_solving_reddit_threads.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Show previous games played')
+
+# Top 5-10 in every category
+
+# Fantasy Corner here?
+# https://www.fantasypros.com/nba/players/advanced-stats.php
+# take the players that went most against expectation: biggest difference between expected and actual points/asts/etc.
+
+# take actual reddit discussions from the teams past x years to see the state of their thoughts on the team, players, front office, etc.
+# Highlights of the last 5 years
+# Memphis Grizzlies
+# Dallas Mavericks
+# Denver Nuggets
+# Could even do the Celtics and Warriors, or last 5 champions? 
+
+# if you run into a semi-interesting (though more likely toxic) thread, you can do some writing on it here and implement some code over time.
