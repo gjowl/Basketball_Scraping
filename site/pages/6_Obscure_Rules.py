96ef58520d29c389262483e7f4d7cb78a83f41bc
gjowl <gjowl04@gmail.com>
2025-04-20 17:43:54 -0400

    working on this as I watch games and adding ideas to find in the rulebook

--- site/pages/6_Obscure_Rules.py
+++ site/pages/6_Obscure_Rules.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 st.title('In the works! A list of secure rules from the NBA rulebook')
 
+st.write('Double fouls: don\'t count as team fouls, but do count as personal fouls.')
+st.write('Offensive interference: also can be used to reward a defending team for their effort: getting a block and the ball goes in to the basket as the shooter holds it.')
+st.write('Why it is offensive interference / that the ball did not go in if the ball does not clear the net?')
 # Top 5-10 in every category
 
 # Fantasy Corner here?
