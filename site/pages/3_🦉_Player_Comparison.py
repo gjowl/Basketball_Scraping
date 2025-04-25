60b9a3f69a2d0f32edfce54043b928310e63e2e4
gjowl <gjowl04@gmail.com>
2025-04-24 21:03:12 -0400

    using multiselect; can compare even more players

--- site/pages/3_🦉_Player_Comparison.py
+++ site/pages/3_🦉_Player_Comparison.py
@@ -30,24 +30,36 @@ team_colors = pd.read_csv(colors)
 option_df = pd.read_csv(options)
 
 # FUNCTIONS
-def compare_player_scatterplot(_playerdf_1, _playerdf_2, _xaxis, _yaxis, n=0):
-    player_name_1, player_name_2 = _playerdf_1['PLAYER_NAME'].values[0], _playerdf_2['PLAYER_NAME'].values[0]
+def compare_player_scatterplot(_player_dfs, _xaxis, _yaxis, n=0):
+    names, hover_templates = [], []
+    for player_df in _player_dfs:
+        names.append(player_df['PLAYER_NAME'].values[0])
     # make the hover template for the player name
     if _yaxis != 'GP':
-        hover_template_1 = player_name_1 + f'<br>{_xaxis}: ' + _playerdf_1[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + _playerdf_1[_yaxis].astype(str) + '<br>GP: ' + _playerdf_1['GP'].astype(str)
-        hover_template_2 = player_name_2 + f'<br>{_xaxis}: ' + _playerdf_2[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + _playerdf_2[_yaxis].astype(str) + '<br>GP: ' + _playerdf_2['GP'].astype(str)
+        for player_df, name in zip(player_dfs, names):
+            hover_template = name + f'<br>{_xaxis}: ' + player_df[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + player_df[_yaxis].astype(str) + '<br>GP: ' + player_df['GP'].astype(str)
+            hover_templates.append(hover_template)
     else:
-        hover_template_1 = player_name_1 + f'<br>{_xaxis}: ' + _playerdf_1[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + _playerdf_1[_yaxis].astype(str)
-        hover_template_2 = player_name_2 + f'<br>{_xaxis}: ' + _playerdf_2[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + _playerdf_2[_yaxis].astype(str)
+        for player_df, name in zip(player_dfs, names):
+            hover_template = name + f'<br>{_xaxis}: ' + player_df[_xaxis].astype(str) + '<br>' + _yaxis + ': ' + player_df[_yaxis].astype(str)
+            hover_templates.append(hover_template)
     # make a scatterplot of the 3P% vs year for both players on the same graph
-    fig = px.scatter(_playerdf_1, x=_xaxis, y=_yaxis, color='PLAYER_NAME', hover_name='PLAYER_NAME')
-    # add in the hover template for the first player
-    fig.add_trace(go.Scatter(x=_playerdf_1[_xaxis], y=_playerdf_1[_yaxis], mode='markers', name=player_name_1, hovertemplate=hover_template_1, marker=dict(color='blue', size=10, line=dict(width=2, color='DarkSlateGrey'))))
-    fig.add_trace(go.Scatter(x=_playerdf_1[_xaxis], y=_playerdf_1[_yaxis], mode='lines', name=player_name_1, line=dict(color='blue', width=2)))
-    # add the second player to the graph
-    fig.add_trace(go.Scatter(x=_playerdf_2[_xaxis], y=_playerdf_2[_yaxis], mode='markers', name=player_name_2, hovertemplate=hover_template_2, marker=dict(color='red', size=10, line=dict(width=2, color='DarkSlateGrey'))))
-    fig.add_trace(go.Scatter(x=_playerdf_2[_xaxis], y=_playerdf_2[_yaxis], mode='lines', name=player_name_2, line=dict(color='red', width=2)))
-    fig.update_traces(marker=dict(size=12, line=dict(width=2, color='DarkSlateGrey')))
+    #fig = px.scatter(player_dfs[0], x=_xaxis, y=_yaxis, color='PLAYER_NAME', hover_name='PLAYER_NAME')
+    #fig.add_trace(go.Scatter(x=player_dfs[0][_xaxis], y=player_dfs[0][_yaxis], mode='lines', name=player_name, hovertemplate=hover_template, marker=dict(color='white', size=18, line=dict(width=2, color='DarkSlateGrey'))))
+    #fig.add_trace(go.scatter(x=player_dfs[0][_xaxis], y=player_dfs[0][_yaxis], mode='lines', name=player_name, line=dict(color='#f27522', width=2)))
+    for player_df, hover_template, player_name in zip(_player_dfs, hover_templates, names):
+        # if the first player, create the fig
+        if player_df['PLAYER_NAME'].values[0] == player_dfs[0]['PLAYER_NAME'].values[0]:
+            fig = px.scatter(player_df, x=_xaxis, y=_yaxis, color='PLAYER_NAME', hover_name='PLAYER_NAME')
+            fig.add_trace(go.Scatter(x=player_df[_xaxis], y=player_df[_yaxis], mode='lines', name=player_name, hovertemplate=hover_template, marker=dict(color='white', size=18, line=dict(width=2, color='DarkSlateGrey'))))
+        else:
+            # add in the hover template for the first player
+            fig.add_trace(go.Scatter(x=player_df[_xaxis], y=player_df[_yaxis], mode='markers', name=player_name, hovertemplate=hover_template, marker=dict(color='white', size=18, line=dict(width=2, color='DarkSlateGrey'))))
+            fig.add_trace(go.Scatter(x=player_df[_xaxis], y=player_df[_yaxis], mode='lines', name=player_name, hovertemplate=hover_template, marker=dict(color='white', size=18, line=dict(width=2, color='DarkSlateGrey'))))
+    ## add the second player to the graph
+    #fig.add_trace(go.Scatter(x=_playerdf_2[_xaxis], y=_playerdf_2[_yaxis], mode='markers', name=player_name_2, hovertemplate=hover_template_2, marker=dict(color='#F27522', size=18, line=dict(width=2, color='DarkSlateGrey'))))
+    #fig.add_trace(go.scatter(x=_playerdf_2[_xaxis], y=_playerdf_2[_yaxis], mode='lines', name=player_name_2, line=dict(color='#f27522', width=2)))
+    fig.update_traces(marker=dict(size=16, line=dict(width=3, color='black')))
     # remove the legend
     fig.update_layout(showlegend=False)
     fig.update_layout(title=f'{_yaxis} per {_xaxis}', xaxis_title=_xaxis, yaxis_title=_yaxis)
@@ -99,75 +111,65 @@ if st.toggle('**Advanced Stats**'):
 # TODO: is it possible to add a list of recommended players to compare? Like a list of players that are similar to the player chosen
 # TODO: write a way that outputs the most important takeaway from the data
 # choose a player from the list of players
-c1, c2 = st.columns(2)
-with c1:
-    st.write('**Choose a player**')
-    # set the default to Stephen Curry
-    player_name = st.selectbox('Player 1', player_names_count.index.tolist(), index=player_names_count.index.tolist().index('Stephen Curry'),key='player_name')
-with c2:
-    st.write('**Choose a second player**')
-    # set the default to Steve Nash
-    player_name_2 = st.selectbox('Player 2', player_names_count.index.tolist(), key='player_name_2', index=player_names_count.index.tolist().index('Steve Nash'))
+players = st.multiselect('**Select players to compare**', player_names_count.index.tolist(), default=['Stephen Curry', 'Steve Nash'], key='players')
 
 # get the data for the selected player
-player_data = player_names[player_names['PLAYER_NAME'] == player_name].reset_index(drop=True)
-player_data_2 = player_names[player_names['PLAYER_NAME'] == player_name_2].reset_index(drop=True)
+player_dfs = []
+for player in players:
+    player_df = player_names[player_names['PLAYER_NAME'] == player].reset_index(drop=True)
+    player_dfs.append(player_df)
 st.divider()
 
 ## TOGGLE TO PLOT BY YEARS IN LEAGUE OR SEASON
 if st.toggle('**Compare by years in league**', key='compare_years', value=True):
     xaxis = 'YEARS_IN_LEAGUE'
-    # get the years in league for each player
-    player_data['YEARS_IN_LEAGUE'] = player_data['SEASON'].astype(int) - player_data['SEASON'].astype(int).min()
-    player_data_2['YEARS_IN_LEAGUE'] = player_data_2['SEASON'].astype(int) - player_data_2['SEASON'].astype(int).min()
+    for player_df in player_dfs:
+        # get the years in league for each player
+        player_df['YEARS_IN_LEAGUE'] = player_df['SEASON'].astype(int) - player_df['SEASON'].astype(int).min()
 else:
     xaxis = 'SEASON'
 
-st.write('By default data are plotted by season, but you can also choose to plot by years in the league by switching the toggle above.')
-    
 # SELECT STATS TO PLOT 
 # TODO: choose what to do if the same player is chosen
 # TODO: add in recommended stats to compare
 # TODO: decide if I should get some averages for each player? Or average overall for all players throughout the years both players played?
 cols = []
-if player_name == player_name_2:
-    st.write('Same player chosen, please choose a different player')
-    # TODO: is there a way to compare Westbrook's late and early stage career? Could be interesting to see clearly hwo he's the same and has changed
-    # TODO: add to this section when time
+# plot the data against each other on the same plot
+st.write('*Choose up to 3 stats to compare*')
+# choose a stat to compare
+if advanced == False:
+    stat_1 = st.selectbox('*Stat 1*', player_dfs[0].columns.tolist()[3:], key='stat_1', index=player_dfs[0].columns.tolist()[3:].index('PPG'))
+    stat_2 = st.selectbox('*Stat 2*', player_dfs[0].columns.tolist()[3:], key='stat_2', index=player_dfs[0].columns.tolist()[3:].index('3P%'))
+    stat_3 = st.selectbox('*Stat 3*', player_dfs[0].columns.tolist()[3:], key='stat_3', index=player_dfs[0].columns.tolist()[3:].index('FG%'))
+    cols = ['PLAYER_NAME', xaxis, 'GP', stat_1, stat_2, stat_3]
 else:
-    # plot the data against each other on the same plot
-    st.write('*Choose up to 3 stats to compare*')
-    # choose a stat to compare
-    if advanced == False:
-        stat_1 = st.selectbox('*Stat 1*', player_data.columns.tolist()[3:], key='stat_1', index=player_data.columns.tolist()[3:].index('PPG'))
-        stat_2 = st.selectbox('*Stat 2*', player_data_2.columns.tolist()[3:], key='stat_2', index=player_data_2.columns.tolist()[3:].index('3P%'))
-        stat_3 = st.selectbox('*Stat 3*', player_data_2.columns.tolist()[3:], key='stat_3', index=player_data_2.columns.tolist()[3:].index('FG%'))
-        cols = ['PLAYER_NAME', 'YEARS_IN_LEAGUE', 'GP', stat_1, stat_2, stat_3]
-    else:
-        stat_1 = st.selectbox('*Stat 1*', player_data.columns.tolist()[3:], key='stat_1', index=player_data.columns.tolist()[3:].index('TS%'))
-        stat_2 = st.selectbox('*Stat 2*', player_data_2.columns.tolist()[3:], key='stat_2', index=player_data_2.columns.tolist()[3:].index('USG%'))
-        stat_3 = st.selectbox('*Stat 3*', player_data_2.columns.tolist()[3:], key='stat_3', index=player_data_2.columns.tolist()[3:].index('AST%'))
-        cols = ['PLAYER_NAME', 'SEASON', 'GP', stat_1, stat_2, stat_3]
-    stats = [stat_1, stat_2, stat_3]
-    st.divider()
+    stat_1 = st.selectbox('*Stat 1*', player_dfs[0].columns.tolist()[3:], key='stat_1', index=player_dfs[0].columns.tolist()[3:].index('TS%'))
+    stat_2 = st.selectbox('*Stat 2*', player_dfs[0].columns.tolist()[3:], key='stat_2', index=player_dfs[0].columns.tolist()[3:].index('USG%'))
+    stat_3 = st.selectbox('*Stat 3*', player_dfs[0].columns.tolist()[3:], key='stat_3', index=player_dfs[0].columns.tolist()[3:].index('AST%'))
+    cols = ['PLAYER_NAME', xaxis, 'GP', stat_1, stat_2, stat_3]
+stats = [stat_1, stat_2, stat_3]
+st.divider()
 
 # keep the first 3 columns and the stat columns
-player_data, player_data_2 = player_data[cols], player_data_2[cols]
+final_dfs = []
+for player_df in player_dfs:
+    player_df = player_df[cols]
+    final_dfs.append(player_df)
 
 # PLOTS
 n = 0
 for stat in stats:
-    compare_player_scatterplot(player_data, player_data_2, xaxis, stat, n)
+    compare_player_scatterplot(final_dfs, xaxis, stat, n)
     n+=1
 
 # add a button to show the player data
 if st.button('Show player data', key='show_player_data'):
     c1, c2 = st.columns(2)
     with c1:
-        st.write('Player 1')
+        st.write(f'{player_name}')
         st.dataframe(player_data, use_container_width=True, hide_index=True)
     with c2:
-        st.write('Player 2')
+        st.write(f'{player_name_2}')
         st.dataframe(player_data_2, use_container_width=True, hide_index=True)
     st.button('Hide player data', key='hide_player_data')
 
@@ -176,4 +178,3 @@ if st.button('Show player data', key='show_player_data'):
 ## - MKG vs Haywood Highsmith
 ## - Matas vs Tatum (from his first year with the type of game he has (percentages and usage and advanced might agree?); let's see if he adds the mid-range and passing next!)
 ## - Daniel Gafford vs Gary Payton II
-# TODO: st.multiselect, st.pills may be a good tool to use for the comparing stats
