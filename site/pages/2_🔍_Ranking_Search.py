7b9b58e8a87e2473a79a273a35b4946fe87cc9f3
gjowl <gjowl04@gmail.com>
2025-04-27 00:22:32 -0400

    think it's basically good, just need to put on some finishing touches (add warnings and setup how the page will work)

--- site/pages/2_🔍_Ranking_Search.py
+++ site/pages/2_🔍_Ranking_Search.py
@@ -90,8 +90,6 @@ def transform_ranks_for_plotting(season_df):
 ## SELECT A PLAYER FROM THE DROPDOWN
 ## TABS SEPARATED STATS AND GRAPHS
 ## BUTTON TO SHOW PLAYER DATA
-# TODO: add an option to just get the player that has the nth ranking of x stat
-## TODO: clean this code up
 # TODO: get a average for all years for each stat and plot as another line; gives context to the player being an outlier or not
 
 # LOAD IN THE DATA
@@ -146,49 +144,33 @@ with tabs[0]:
 # TAB 2: STAT SEARCH
 ## SELECT A STAT TO PLOT
 # get the top x players for the stat
+output_df = pd.DataFrame()
 with tabs[1]:
-    st.write('**Select a stat to plot**')
+    #st.write('**PLAYER** is **#RANK** in **STAT**, averaging **VAL** for the **SEASON**')
     all_rank_cols = pd.concat(all_ranks, ignore_index=True)
     df = all_ranks[0]
     stat_list = all_rank_cols.columns[all_rank_cols.columns.str.contains('_Rank')].tolist()
     # remove rank from the stat list
     stat_list = [stat.split('_Rank')[0] for stat in stat_list]
-    stat = st.selectbox('Select a stat to plot', stat_list, index=None, placeholder='Stat Name...')
-    season = st.selectbox('Select the season of interest', df['YEAR'].unique(), key=f'season_{plot_number}')
+    stat = st.selectbox('Select a stat', stat_list, index=None, placeholder='Stat Name...')
+    season = st.selectbox('Select the season', df['YEAR'].unique(), key=f'season_{plot_number}')
     season_df = df[df['YEAR'] == season].reset_index(drop=True)
-    # get the top x players for the stat
-    top_x = st.slider('Number of players to plot', 1, 25, 10)
     rank, percentile = f'{stat}_Rank', f'{stat}_Percentile'
-    # keep only the players with the top x of the stat for each year
-    season_df = season_df.sort_values(by=percentile, ascending=False).groupby('YEAR').head(top_x)
-    season_df.reset_index(drop=True, inplace=True)
-    # get the player rankings for the stat
-    #output_df = season_df[['PLAYER_NAME', f'{stat}']]
-    #stat_ranks = season_df[stat]
-    # plot a bar graph of the top x players for the stat
-    fig = px.bar(season_df, x=season_df['PLAYER_NAME'], title = f'{stat} Ranks', y=season_df[percentile], labels={'x': 'Stat', 'y': 'Percentile'})
-    # add the rank above each bar
-    for i in range(len(season_df)):
-        # if ranking is NaN, skip it
-        if pd.isna(season_df[rank][i]):
-            continue
-        fig.add_annotation(x=i, y=season_df[percentile][i], text=f'#{int(season_df[rank][i])}', showarrow=False, font=dict(size=16), yshift=10)
-    #fig = px.bar(season_df.nlargest(top_x, stat), x='PLAYER_NAME', y=stat, color='TEAM_ABBREVIATION', title=f'Top {top_x} Players for {stat} in {season}')
-    # remove the x-axis title
-    fig.update_xaxes(title='')
-    fig.update_yaxes(title='')
-    # set the x-axis label size
-    # remove the y-axis lines, title, and ticks
-    fig.update_layout(yaxis=dict(showgrid=False, zeroline=False, showticklabels=False), xaxis=dict(showgrid=False, zeroline=False, showticklabels=True))
-    fig.update_yaxes(showline=False, title='', ticks='', showticklabels=False)
-    fig.update_xaxes(tickfont=dict(size=16))
-    fig.update_layout(font_family="monospace")
-    # TODO: make the hover the stat
-    #fig.update_traces(hovertemplate=season_df['PLAYER_NAME'] + f'<br>{stat}: ' + season_df[stat].astype(str))
-    st.plotly_chart(fig, use_container_width=True)
-    # 
-    st.dataframe(output_df, use_container_width=True, hide_index=True)
-    plot_number += 1
+    # get a list of the number of ranks in the league
+    rank_list = sorted(season_df[rank].unique(), reverse=False)
+    # convert the rank list to a list of int
+    rank_list = [int(rank) for rank in rank_list if str(rank) != 'nan']
+    rank_num = st.selectbox('Select a rank', rank_list, index=None, placeholder='Rank...')
+    # get the x ranked player
+    player = season_df[season_df[rank] == rank_num]['PLAYER_NAME'].values[0]
+    # get the actual stat value for the player
+    data_df = year_data_dict[season]
+    stat_value = data_df[data_df['PLAYER_NAME'] == player][stat].values[0]
+    # add the values to the df 
+    tmp_df = pd.DataFrame({'PLAYER_NAME': [player], 'RANK': [rank_num], 'STAT': [stat], 'VALUE': [stat_value], 'SEASON': [season]})
+    #output_df = pd.concat([output_df, tmp_df], ignore_index=True)
+    st.write(f'In the **{season}** season, **{player}** averaged **{round(stat_value,2)}**  **{stat}** good for **#{rank_num}** in the league.')
+    # TODO: if possible, make this like queereable where it shows up to the last x searches (like a search history)
 with tabs[2]:
     st.write('**Select a year to plot**')
     # get the top x players for the stat
@@ -200,6 +182,7 @@ with tabs[2]:
 
 if st.button(f'Show All {player} Data'):
     # show all the data with no scroll bar
+    player_df = get_player_data(year_data_dict, player)
     st.dataframe(player_df, use_container_width=True, hide_index=True)
     st.button('Hide Data', key=f'hide_{player}_data')
 # an interesting alternate idea (or maybe concurrent) is to basically make the website a scrolling timeline of the player: Kind of like the spotify wrapped, but a timeline of the player with 
