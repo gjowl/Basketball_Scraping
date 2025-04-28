232a041b51033c42cc83d3f4642bcf978c218019
gjowl <gjowl04@gmail.com>
2025-04-27 20:28:09 -0400

    updates on blurbs

--- site/pages/5_🔵_Blue_Moons.py
+++ site/pages/5_🔵_Blue_Moons.py
@@ -15,6 +15,9 @@ leaders_dir = '/mnt/d/github/Basketball_Scraping/site/leaders/'
 
 # Page Setup
 
+st.write('''
+         The stats that occur "once in a blue moon" are stats that rarely occur and that go down in the history books.\n
+         ''')
 # TODO: add in a blurb about the data being from Wikipedia with a link to the pages
 # TODO: need to work on the analysis here. If no time, at least something simple
 # get the list of csv files in the directory
@@ -38,28 +41,6 @@ if st.toggle("Show Simplified DataFrames", key="show_df", value=True):
     for tab, csv in zip(tabs,csv_names):
         with tab:
             cols = ['Player', csv, 'Date']
-            ## check if the csv is 'REB' and if so, add the 'Result' column
-            #if csv != 'REB':
-            #    cols.append('Result')
-            ## if the csv is '3PM', add the '3PA' column next to it
-            #if csv == '3PM':
-            #    cols.insert(2, '3PA')
-            #    # graph the 3PM vs 3PA as an altair chart with wins and losses as a bar graph below
-            #    points = (alt.Chart(dfs[csv])
-            #        .mark_circle(size=60, opacity=0.5, color='blue')
-            #        .encode(x='3PA', y='3PM', tooltip=['Player', '3PM', '3PA', 'Date', 'Result'])
-            #        .properties(width=600, height=400)
-            #    )
-            #    bars = (alt.Chart(dfs[csv])
-            #        .mark_bar(opacity=0.5)
-            #        .encode(x='Result', y='count(Result)')
-            #        .properties(width=600, height=200)
-            #    )
-            #    chart = alt.vconcat(points, bars, spacing=5).resolve_scale(y='independent')
-            #    chart = chart.configure_axisX(tickCount=10).configure_axisY(tickCount=11)
-            #    # change the scale of the x axis to be 0-20 and the y axis to be 0-20
-            #    
-            #    st.altair_chart(chart, use_container_width=True)
             df = dfs[csv][cols]
             st.dataframe(df, use_container_width=True, hide_index=True)
 else:
@@ -67,30 +48,3 @@ else:
         with tab:
             df = dfs[csv]
             st.dataframe(df, use_container_width=True, hide_index=True)
-
-
-# create a dictionary to hold the dataframes
-# rare events...what can I add here? 
-# Most 3pt attempts in a game 
-# Most 3pt makes in a game
-# Most assists in a game 
-# Most rebounds in a game
-# Most steals in a game
-# Most blocks in a game
-# Most turnovers in a game
-# Most minutes in a game
-
-# The unlikely to ever happen ones
-# Most fouls in a game
-# Most points in a game
-# Most games played in a season
-# Most games played in a career
-# Most games played in a month
-
-# script compares to the current csv file, checks these cats, then updates the main csv if is different (adds in the date, previous change, etc.)
-# this is mostly just a look up table; so I think I'm just going to get those tomorrow when all games are going
-
-# Citations:
-
-# altair chart? for wins/losses; 3pm vs 3pa and w/l
-#
--- site/pages/6_❓_About.py
+++ site/pages/6_❓_About.py
@@ -7,57 +7,56 @@ st.title("About")
 st.write(
     """
     I put this website together to help me learn about web development and
-    data scraping, allowing me to practice dynamic data visualization. \n
+    data scraping, allowing me to practice dynamic data visualization.\n
     For years I've been interested in the NBA and taking boxscore data and
-    finding a better way to visualize it.\n
-    I hope you enjoy looking through the data, seeing some of the graphs and stats,
-    and maybe even learning something new about the game.\n
+    finding a way to visualize it for myself\n
+    Data is sourced from [NBA.com](https://www.nba.com/stats/leaders) going back to the 1996-97 season.\n
     If you have any questions, comments, or suggestions, please feel free to reach out: gjowl04@gmail.com!\n
     """
 )
+st.divider()
 
 ## PAGE 1
 st.write(
     """
     🔝 Season Stats
     ===========\n
-    This page allows you to view the top players in the league for a given season.
+    This page allows you to view the **top** players in the league for a **chosen season**.
     """
 )
+st.divider()
 
 ## PAGE 2
 st.write(
     """
     🔍 Player Rankings
     ===========\n
-    You can search for a player and view their statistical rankings all time and/or for a given season. \n
-    It also includes a Ranking Search, where you can search for a rank and output the player either all time and/or for a given season.\n
+    You can search for a player and view their statistical rankings **OR** search for a rank and output the player.\n
+    Both searches can be made for a **chosen season** or **all time**.\n
     """
 )
+st.divider()
 
 ## PAGE 3
 st.write(
     """
     🦉 Player Comparison
     ===========\n
-    This page allows you to compare the stats of players over the years they have played in the league since the 1996-97 season (as far back as nba.com has data).
+    This page allows you to compare the stats of players over the years they have played in the league.
     """
 )
+st.divider()
 
 ## PAGE 4
 st.write(
     """
     📈 Yearly Graphs 
     ===========\n
-    You can filter the data by number of years in the league and games played to see how players have performed over their years in the league.
+    You can filter the data by number of years in the league and games played to see how players have performed over their years in the league. \n
+    Eventually, I'd like to update this page to identify trends over time according to these data.
     """
 )
-
-st.write(
-    """
-    All of the data for the above pages is sourced from [NBA.com](https://www.nba.com/stats/leaders).
-    """
-)
+st.divider()
 
 ## PAGE 5
 st.write(
@@ -65,12 +64,12 @@ st.write(
     🔵 Blue Moons 
     ===========\n
     A page dedicated to players who have had rare regular season performances that have stood the test of time in NBA records.\n
-    All of the data for this page is sourced from the Wikipedia pages below.\n
-    [3PM](https://en.wikipedia.org/wiki/List_of_NBA_single-game_3-point_scoring_leaders)\n
-    [Points](https://en.wikipedia.org/wiki/List_of_NBA_single-game_scoring_leaders)\n
-    [Assists](https://en.wikipedia.org/wiki/List_of_NBA_single-game_assists_leaders)\n
-    [Rebounds](https://en.wikipedia.org/wiki/List_of_NBA_single-game_rebounding_leaders)\n
-    [Blocks](https://en.wikipedia.org/wiki/List_of_NBA_single-game_blocks_leaders)\n
+    I always wanted to be able to look at the top stats all time, and was able to create this page using data from the Wikipedia pages below.\n 
+    [3PM](https://en.wikipedia.org/wiki/List_of_NBA_single-game_3-point_scoring_leaders) 
+    [Points](https://en.wikipedia.org/wiki/List_of_NBA_single-game_scoring_leaders) 
+    [Assists](https://en.wikipedia.org/wiki/List_of_NBA_single-game_assists_leaders) 
+    [Rebounds](https://en.wikipedia.org/wiki/List_of_NBA_single-game_rebounding_leaders) 
+    [Blocks](https://en.wikipedia.org/wiki/List_of_NBA_single-game_blocks_leaders) 
     """
 )
 
